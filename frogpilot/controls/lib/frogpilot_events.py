dca7174964f7786ba4b6263b3487b46e3dd43f6e
James <91348155+FrogAi@users.noreply.github.com>
2025-10-04 11:17:43 -0700

    Green Light Alert

--- frogpilot/controls/lib/frogpilot_events.py
+++ frogpilot/controls/lib/frogpilot_events.py
@@ -2,6 +2,8 @@
 from openpilot.common.params import Params
 from openpilot.selfdrive.selfdrived.events import ET, EVENT_NAME, FROGPILOT_EVENT_NAME, EventName, FrogPilotEventName, Events
 
+from openpilot.frogpilot.common.frogpilot_variables import NON_DRIVING_GEARS
+
 class FrogPilotEvents:
   def __init__(self, FrogPilotPlanner, error_log):
     self.params = Params()
@@ -12,6 +14,7 @@ class FrogPilotEvents:
     self.events = Events(frogpilot=True)
 
     self.startup_seen = False
+    self.stopped_for_light = False
 
     self.max_acceleration = 0
 
@@ -33,6 +36,14 @@ class FrogPilotEvents:
       self.max_acceleration = max(acceleration, self.max_acceleration)
     else:
       self.max_acceleration = 0
+
+    if not self.frogpilot_planner.tracking_lead and sm["carState"].standstill and sm["carState"].gearShifter not in NON_DRIVING_GEARS:
+      if not self.frogpilot_planner.model_stopped and self.stopped_for_light and frogpilot_toggles.green_light_alert:
+        self.events.add(FrogPilotEventName.greenLight)
+
+      self.stopped_for_light = self.frogpilot_planner.frogpilot_cem.stop_light_detected
+    else:
+      self.stopped_for_light = False
 
     if self.error_log.is_file():
       self.events.add(FrogPilotEventName.openpilotCrashed)
