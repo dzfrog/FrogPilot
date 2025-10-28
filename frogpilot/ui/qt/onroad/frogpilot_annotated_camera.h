a0881ffa427fba9345fc973429b30d1d9f3ca179
James <91348155+FrogAi@users.noreply.github.com>
2025-10-28 02:03:25 -0700

    Blind spot path

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
@@ -40,6 +40,8 @@ protected:
   void showEvent(QShowEvent *event) override;
 
 private:
+  void paintBlindSpotPath(QPainter &p, SubMaster &sm, SubMaster &fpsm);
+
   float distanceConversion;
   float setSpeed;
   float speedConversion;
