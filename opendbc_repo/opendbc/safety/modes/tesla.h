d163c4f06c6194782a74da741f07ce75bfdc6d26
James <91348155+FrogAi@users.noreply.github.com>
2025-10-29 10:58:43 -0700

    Always On Lateral
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- opendbc_repo/opendbc/safety/modes/tesla.h
+++ opendbc_repo/opendbc/safety/modes/tesla.h
@@ -161,6 +161,7 @@ static void tesla_rx_hook(const CANPacket_t *msg) {
       pcm_cruise_check(cruise_engaged);
 
       // FrogPilot variables
+      acc_main_on = cruise_engaged;
     }
 
     if (msg->addr == 0x155U) {
