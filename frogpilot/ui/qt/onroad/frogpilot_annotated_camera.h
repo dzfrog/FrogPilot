ae5198aee4f8daf0ddaa437bae748cbf89ff1666
James <91348155+FrogAi@users.noreply.github.com>
2025-11-23 22:53:36 -0700

    Display the model's stopping point

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
@@ -46,6 +46,7 @@ private:
   void paintCurveSpeedControlTraining(QPainter &p, SubMaster &fpsm);
   void paintRoadName(QPainter &p);
   void paintStandstillTimer(QPainter &p);
+  void paintStoppingPoint(QPainter &p, SubMaster &sm);
   void paintTurnSignals(QPainter &p, SubMaster &sm);
   void updateSignals();
 
@@ -74,6 +75,7 @@ private:
   QElapsedTimer standstillTimer;
 
   QPixmap curveSpeedIcon;
+  QPixmap stopSignImg;
 
   QPoint cemStatusPosition;
   QPoint compassPosition;
